c41d01b660ef884a2fea352664ada5e942811ffb
Alexey <cappuccino13371@gmail.com>
2023-09-10 10:10:46 +0300

    init repo and starts files

---
+++ Program.java
@@ -0,0 +1,20 @@
+import models.TableModel;
+import presenters.BookingPresenter;
+import views.BookingView;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+        TableModel model = new TableModel();
+        BookingView view = new BookingView();
+        BookingPresenter bookingPresenter = new BookingPresenter(model, view);
+        bookingPresenter.updateTablesUI();
+
+        view.reservationTable(new Date(), 3, "Станислав");
+
+        view.changeReservationTable(101, new Date(), 1, "Станислав");
+    }
+
+}
